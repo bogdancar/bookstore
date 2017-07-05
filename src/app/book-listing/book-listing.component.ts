d33cbbb3709219332c5a13832b02be26272dd485
EgomortIncognitus <milos.milojevic.misa202@gmail.com>
2017-07-05 10:34:47 +0200

    third commit

--- src/app/book-listing/book-listing.component.ts
+++ src/app/book-listing/book-listing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { BooksService } from './../services/books.service';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-book-listing',
@@ -12,6 +11,10 @@ export class BookListingComponent implements OnInit {
 
   books: Array<any>;
   error: string;
+  sortField: Array<string> = [
+    'name',
+    'price'
+  ];
 
   constructor(
     private http: Http,
